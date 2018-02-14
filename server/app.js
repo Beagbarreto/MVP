96de0f53454b0c676c994b2c757edcf77b9208b1
Beagbarreto <beagbarreto09@gmail.com>
2018-02-14 17:10:48 -0600

    structure of index.jsx in client

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
-
+//var database = require('/');
 var app = express();
 
 //midleware goes here
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.send("I've created a monster");
 });
 
 app.listen(3000, function(){
-  console.log("It's alive!...");
+  console.log("It's alive!");
 })
