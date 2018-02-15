52f5fa3120768c63639f6ac9f13f0e949ff5483e
Beagbarreto <beagbarreto09@gmail.com>
2018-02-15 16:46:41 -0600

    create database files

---
+++ database/data.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ActList: [
+                {id: 1, day: "01/16/18", title: "Chinese New Year", hour: "21:00", location: "Asian Green Cuisine", description: "Dumplings bring abundance for Lunar Year"},
+                {id: 2, day: "01/17/18", title: "Black Panther Movie", hour: "19:45", location: "WTC movie theater", description: "New Marvel movie"}
+              ]
+};
