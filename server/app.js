1d73d04e33dc57b8a1b9d2510a38f86fae9360a3
Beagbarreto <beagbarreto09@gmail.com>
2018-02-14 22:34:30 -0600

    fix problem with json and missing folders

--- server/app.js
+++ server/app.js
@@ -6,6 +6,15 @@ var app = express();
 //midleware goes here
 app.use(bodyParser.json());
 
+var users = [{
+  name: 'Eduardo'
+  e-mail: 'urrutia.e14@gmail.com'
+},
+{
+  name: 'Miri'
+  e-mail: 'unicorns@gmail.com'
+}];
+
 app.get('/', function(req, res){
   res.render('index');
 });
